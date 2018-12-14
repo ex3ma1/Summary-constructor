afb5499980bed7e8b3889fb6318414e6f50cc63d
kostia <kostia.olejnik2@gmail.com>
2018-12-14 02:09:09 +0200

    template selectin functionality

--- sv.php
+++ sv.php
@@ -16,12 +16,44 @@
 
 
 <div class="w3-bar w3-black">
+  <button class="w3-bar-item w3-button" onclick="openCity('template')">Templates</button>
   <button class="w3-bar-item w3-button" onclick="openCity('London')">London</button>
   <button class="w3-bar-item w3-button" onclick="openCity('Paris')">Paris</button>
+  
+
   <button class="w3-bar-item w3-button" onclick="openCity('Tokyo')">Tokyo</button>
+
+  <a class="w3-bar-item w3-button" href="logout.php">Log out</a>
 </div>
 
-<div id="London" class="w3-container city">
+<div id="template" class="w3-container city" >
+  <h2>Templates</h2>
+  <div>
+  <form id='raz'>
+        <div>
+          <input type="radio" checked name="raz" value="block"/> 
+          <div style="background: red; width:100px; height:100px"></div>  
+        </div>
+          <br>
+        <div>
+          <input type="radio" checked name="raz" value="block2"/> 
+          <div style="background: black; width:100px; height:100px"></div>  
+        </div>
+          <br>
+        <div>
+          <input type="radio" checked name="raz" value="block3"/> 
+          <div style="background: green; width:100px; height:100px"></div>  
+        </div>
+
+        <br>
+
+     
+  </form>
+  </div>
+
+</div>
+
+<div id="London" class="w3-container city" style="display:none">
   <h2>London</h2>
   <div>
       <input type="text" id="name">
@@ -48,15 +80,36 @@
 
 <div id="Tokyo" class="w3-container city" style="display:none">
   <div id="sv">
-    <h2>SV</h2>
-    <p> Name: </p>
-    <p id="block"> </p>
-    <div id='avatar'></div>
+      <div id="Label1" style='display: none;'>
+          <h2>Template 1</h2>
+          
+          <p id="block"> </p>
+          <div id='avatar_block'></div>
+      
+     </div>
+
+     <div id="Label2" style='display: none;'>
+     Template 2
+     <p id="block2"> </p>
+     <div id='avatar_block2'></div>
+     </div>
+
+     <div id="Label3" style='display: none;'>
+     Template 3
+     <p id="block3"> </p>
+     <div id='avatar_block3'></div>
+     </div>
+         
+      
   </div>
   <a href="javascript:genPDF()">Save</a>
+
+
+
+    
 </div>
 
-<a href="logout.php">Log out</a>
+
 
 
 
